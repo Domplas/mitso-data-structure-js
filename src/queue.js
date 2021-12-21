07a98f3550285aa8a1aa74cce8dd959d5ef47823
Domplas <90716480+Domplas@users.noreply.github.com>
2021-12-21 14:16:17 +0300

    2.queues

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require("../extensions/list-node.js");
+
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
-
+ module.exports = class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      if (!this.tail) {
+        this.head.next = node;
+        this.tail = node;
+      }
+      this.tail.next = node;
+      this.tail = node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    {
+      if (!this.head) 
+      {
+        return;
+      }
+      const top = this.head;
+      if (this.head === this.tail) 
+      {
+        this.tail = null;
+      }
+      this.head = this.head.next;
+      return top.value;
+    }
   }
-
 }
